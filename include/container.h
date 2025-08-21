4ebb0df59d0989cd1e4c451f0f8b007250f87232
xingwuyang <xingwuyang@tencent.com>
2025-08-21 14:20:05 +0800

    feat: add container funcs

---
+++ include/container.h
@@ -0,0 +1,31 @@
+#ifndef __CONTAINER_H__
+#define __CONTAINER_H__
+
+#include <sys/types.h>
+
+enum {
+    // The stack size for the container
+    CONTAINER_STACK_SIZE = (1024 * 1024),
+};
+
+// Represents the configuration for a container.
+typedef struct {
+    uid_t uid;
+    int fd;
+    char *hostname;
+    char *cmd;
+    char *arg;
+    char *mnt;
+} container_config;
+
+// Initializes the container.
+int container_init(container_config *config, char *stack);
+
+// Waits for the container to exit.
+int container_wait(int container_pid);
+
+// Stops the container.
+void container_stop(int container_pid);
+
+
+#endif
